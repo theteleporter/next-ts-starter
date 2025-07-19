e9893da9383bb0d0d7f269885f0aacaf4bc2a041
The Teleporter <m6253272@gmail.com>
2025-07-19 13:33:09 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,8 +14,8 @@ const inter = Inter({ subsets: ['latin'], variable: '--font-body' })
 
 export const metadata: Metadata = {
   title: {
-    default: 'next-typescript | basement.studio',
-    template: '%s | basement.studio'
+    default: 'next-typescript | theteleporter.site',
+    template: '%s | theteleporter.site'
   },
   metadataBase: siteURL,
   description: `A minimalist's boilerplate — Next.js with TypeScript.`,
@@ -28,9 +28,9 @@ export const metadata: Metadata = {
   manifest: '/manifest.webmanifest',
   twitter: {
     card: 'summary_large_image',
-    title: 'next-typescript | basement.studio',
-    creator: '@basementstudio',
-    siteId: '@basementstudio'
+    title: 'next-typescript | theteleporter.site',
+    creator: '@theteleporter_',
+    siteId: '@theteleporter_'
   }
 }
 
